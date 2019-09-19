e8cad06d19397c883fe1c155d38bf30535a20815
shjang7 <lucy.sh.jang@gmail.com>
2019-09-20 00:15:18 +0900

    Update js with linter check

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,9 +1,9 @@
 const myLibrary = [];
 
 const readNextStatus = Object.freeze({
-  "unread":           "current-reading",
-  "current-reading":  "read",
-  "read":             "unread"
+  unread: 'current-reading',
+  'current-reading': 'read',
+  read: 'unread',
 });
 
 function Book(title, author, pages, read) {
@@ -13,34 +13,34 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-Book.prototype.toggleRead = function() {
+Book.prototype.toggleRead = function () {
   this.read = readNextStatus[this.read];
-}
+};
 
 function getBookInfoFromForm() {
   return {
-    title: document.getElementById("title-form").value,
-    author: document.getElementById("author-form").value,
-    pages: document.getElementById("pages-form").value,
-    read: document.getElementById("read-form").value
-  }
+    title: document.getElementById('title-form').value,
+    author: document.getElementById('author-form').value,
+    pages: document.getElementById('pages-form').value,
+    read: document.getElementById('read-form').value,
+  };
 }
 
 // append myLibrary
 function addBookToLibrary(book) {
-  let newBook = new Book(book.title, book.author, book.pages, book.read);
+  const newBook = new Book(book.title, book.author, book.pages, book.read);
   myLibrary.push(newBook);
 }
 
 function deleteRemains() {
-  const booksContainer = document.querySelector(".books-container");
-  if(booksContainer) {
+  const booksContainer = document.querySelector('.books-container');
+  if (booksContainer) {
     booksContainer.parentNode.removeChild(booksContainer);
   }
 }
 
 function deleteBookFromLibrary(book) {
-  let deleteItem = myLibrary.indexOf(book);
+  const deleteItem = myLibrary.indexOf(book);
   myLibrary.splice(deleteItem, 1);
   render();
 }
@@ -49,32 +49,30 @@ function deleteBookHelper(book, node) {
   const deleteButton = document.createElement('button');
   deleteButton.classList.add('delete-btn');
   deleteButton.classList.add('btn-info');
-  deleteButton.innerHTML = "Delete";
+  deleteButton.innerHTML = 'Delete';
   node.insertAdjacentElement('beforeend', deleteButton);
-  deleteButton.addEventListener("click", () => deleteBookFromLibrary(book));
+  deleteButton.addEventListener('click', () => deleteBookFromLibrary(book));
 }
 
 function updateReadStatus(book, node) {
-  node.addEventListener("click", function() {
+  node.addEventListener('click', () => {
     book.toggleRead();
     render();
-  })
+  });
 }
 
 function readStatusWordsExchange(sentence) {
   let words = sentence.split('-');
-  words = words.map(function(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  });
+  words = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
   return words.join(' ');
 }
 
 function render() {
-  const mainContainer = document.getElementById("main-container");
+  const mainContainer = document.getElementById('main-container');
   // delete original objects
   deleteRemains();
   // if no book, no adding html tag
-  if(myLibrary.length == 0) return;
+  if (myLibrary.length == 0) return;
 
   const booksContainer = document.createElement('section');
   booksContainer.classList.add('books-container');
@@ -88,50 +86,52 @@ function render() {
     node.innerHTML = `Title: ${book.title} |
                       Author: ${book.author} |
                       pages: ${book.pages} |
-                      read status: ${readStatusWordsExchange(book.read)}`
+                      read status: ${readStatusWordsExchange(book.read)}`;
     // delete book button
     deleteBookHelper(book, node);
     // node mouse over description
-    node.title = "Click to set status unread / current reading / read";
+    node.title = 'Click to set status unread / current reading / read';
     // toggle to change read status
     updateReadStatus(book, node);
-  })
+  });
 }
 
 const visiblilityNextStatus = {
-  "visible": "hidden",
-  "hidden": "visible"
-}
+  visible: 'hidden',
+  hidden: 'visible',
+};
 
 function exchangeVisibility() {
-  const addBookForm = document.querySelector(".add-book-form");
-  let style = window.getComputedStyle ? getComputedStyle(addBookForm, null) : addBookForm.currentStyle;
-  addBookForm.style.visibility = visiblilityNextStatus[ style.visibility ];
+  const addBookForm = document.querySelector('.add-book-form');
+  const style = window.getComputedStyle ? getComputedStyle(addBookForm, null) : addBookForm.currentStyle;
+  addBookForm.style.visibility = visiblilityNextStatus[style.visibility];
 }
 
 // window addBookBtn click
 // set form visible
-const addBookBtn = document.getElementById("add-book-btn");
-addBookBtn.addEventListener("click", () => exchangeVisibility())
+const addBookBtn = document.getElementById('add-book-btn');
+addBookBtn.addEventListener('click', () => exchangeVisibility());
 
 // window submitBtn
 // call addBookToLibrary
 // call render
-const submitBtn = document.getElementById("submit-btn");
-submitBtn.addEventListener("click", function() {
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', () => {
   const book = getBookInfoFromForm();
   addBookToLibrary(book);
   exchangeVisibility();
   render();
-})
+});
 
 // window ready
 // call render
 // in case the document is already rendered
-if (document.readyState!='loading') render();
+if (document.readyState != 'loading') render();
 // modern browsers
 else if (document.addEventListener) document.addEventListener('DOMContentLoaded', render);
 // IE <= 8
-else document.attachEvent('onreadystatechange', function(){
-    if (document.readyState=='complete') render();
-});
+else {
+  document.attachEvent('onreadystatechange', () => {
+    if (document.readyState == 'complete') render();
+  });
+}
