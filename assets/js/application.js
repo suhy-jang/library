28a6122cab6ba82778abca36a85900723e15af2d
shjang7 <lucy.sh.jang@gmail.com>
2019-10-01 23:38:38 +0900

    Refactor with class form

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,150 +1,152 @@
 /* eslint-env browser */
-const myLibrary = [];
 
-const description = Object.freeze({
-  toggle: 'Click to set status Unread / Current Reading / Read',
-});
-
-const readNextStatus = Object.freeze({
+const nextReadStatus = Object.freeze({
   unread: 'current_reading',
   current_reading: 'read',
   read: 'unread',
 });
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this._title = title;
+    this._author = author;
+    this._pages = pages;
+    this._read = read;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+
+  get read() {
+    return this._read;
+  }
+
+  toggleRead() {
+    this._read = nextReadStatus[this._read];
+  }
 }
 
-Book.prototype.toggleRead = function setNextStatus() {
-  this.read = readNextStatus[this.read];
-};
-
-function getBookInfoFromForm() {
+const myLibrary = (() => {
+  const resetBooks = () => {
+    this.books = [];
+  };
+  const getBooks = () => this.books;
+  const addBook = newBook => this.books.push(newBook);
+  const removeBook = (book) => {
+    const deleteItem = this.books.indexOf(book);
+    this.books.splice(deleteItem, 1);
+  };
   return {
+    resetBooks, getBooks, addBook, removeBook,
+  };
+})();
+
+const uiController = (() => {
+  const currentReadStatus = Object.freeze({
+    unread: 'Unread',
+    current_reading: 'Current Reading',
+    read: 'Read',
+  });
+
+  const toggleVisiblility = Object.freeze({
+    visible: 'hidden',
+    hidden: 'visible',
+  });
+
+  const clickDescription = 'Click to set status Unread / Current Reading / Read';
+
+  const resetDisplay = () => {
+    const item = document.querySelector('.books-container');
+    if (item) item.parentNode.removeChild(item);
+  };
+
+  const getBookInfoFromForm = () => ({
     title: document.getElementById('title-form').value,
     author: document.getElementById('author-form').value,
     pages: document.getElementById('pages-form').value,
     read: document.getElementById('read-form').value,
+  });
+
+  const bookInfo = ({
+    title, author, pages, read,
+  }) => `Title: ${title} |
+         Author: ${author} |
+         Pages: ${pages} |
+         Read status: ${currentReadStatus[read]}`;
+
+  const createHtmlElement = (type, classes, text) => {
+    const item = document.createElement(type);
+    classes.forEach(name => item.classList.add(name));
+    item.innerHTML = text;
+    return item;
   };
-}
 
-// append myLibrary
-function addBookToLibrary(book) {
-  const newBook = new Book(book.title, book.author, book.pages, book.read);
-  myLibrary.push(newBook);
-}
+  const render = () => {
+    const mainContainer = document.getElementById('main-container');
+    resetDisplay();
+    const books = myLibrary.getBooks();
+    if (books.length === 0) return;
 
-function deleteRemains() {
-  const booksContainer = document.querySelector('.books-container');
-  if (booksContainer) {
-    booksContainer.parentNode.removeChild(booksContainer);
+    const booksContainer = createHtmlElement('section', ['books-container'], '');
+    mainContainer.insertAdjacentElement('afterbegin', booksContainer);
+
+    books.forEach((book) => {
+      // each book
+      const node = createHtmlElement('article', ['book'], bookInfo(book));
+      booksContainer.appendChild(node);
+      // delete button
+      const deleteButton = createHtmlElement('button', ['delete-btn', 'btn-info'], 'Delete');
+      node.insertAdjacentElement('beforeend', deleteButton);
+      deleteButton.addEventListener('click', () => {
+        myLibrary.removeBook(book);
+        render();
+      });
+      // description
+      node.title = clickDescription;
+      node.addEventListener('click', () => {
+        book.toggleRead();
+        render();
+      });
+    });
+
+    const toggleDescription = createHtmlElement('i', ['description-toggle'], clickDescription);
+    booksContainer.appendChild(toggleDescription);
+  };
+
+  const swapVisibility = (select) => {
+    const form = document.querySelector(select);
+    const currentVisibility = window.getComputedStyle(form, null).visibility;
+    form.style.visibility = toggleVisiblility[currentVisibility];
+  };
+
+  const customAddEvent = (id, callback) => {
+    const obj = document.getElementById(id);
+    obj.addEventListener('click', callback);
+  };
+
+  const submitAction = () => {
+    const {
+      title, author, pages, read,
+    } = getBookInfoFromForm();
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.addBook(newBook);
+    swapVisibility('.add-book-form');
+    render();
   }
-}
 
-function deleteBookFromLibrary(book) {
-  const deleteItem = myLibrary.indexOf(book);
-  myLibrary.splice(deleteItem, 1);
-}
+  customAddEvent('add-book-btn', () => swapVisibility('.add-book-form'));
+  customAddEvent('submit-btn', () => submitAction());
+  return { render };
+})();
 
-function deleteButtonInfo() {
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('delete-btn');
-  deleteButton.classList.add('btn-info');
-  deleteButton.innerHTML = 'Delete';
-  return deleteButton;
-}
-
-function readStatusWordsExchange(sentence) {
-  let words = sentence.split(/[^A-Za-z0-9]/);
-  words = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
-  return words.join(' ');
-}
-
-function getDescription() {
-  const descriptTag = document.createElement('i');
-  descriptTag.classList.add('description-toggle');
-  descriptTag.innerHTML += description.toggle;
-  return descriptTag;
-}
-
-function render() {
-  const mainContainer = document.getElementById('main-container');
-  // delete original objects
-  deleteRemains();
-  // if no book, no adding html tag
-  if (myLibrary.length === 0) return;
-
-  const booksContainer = document.createElement('section');
-  booksContainer.classList.add('books-container');
-  mainContainer.insertAdjacentElement('afterbegin', booksContainer);
-
-  // render each book with loop
-  myLibrary.forEach((book) => {
-    const node = document.createElement('article');
-    node.classList.add('book');
-    booksContainer.appendChild(node);
-    node.innerHTML = `Title: ${book.title} |
-                      Author: ${book.author} |
-                      pages: ${book.pages} |
-                      read status: ${readStatusWordsExchange(book.read)}`;
-    // delete book button
-    const deleteButton = deleteButtonInfo();
-    node.insertAdjacentElement('beforeend', deleteButton);
-    deleteButton.addEventListener('click', () => {
-      deleteBookFromLibrary(book);
-      render();
-    });
-    // node mouse over description
-    node.title = description.toggle;
-    // toggle to change read status
-    node.addEventListener('click', () => {
-      book.toggleRead();
-      render();
-    });
-  });
-  booksContainer.appendChild(getDescription());
-}
-
-const visiblilityNextStatus = {
-  visible: 'hidden',
-  hidden: 'visible',
-};
-
-function exchangeVisibility() {
-  const addBookForm = document.querySelector('.add-book-form');
-  const style = window.getComputedStyle
-    ? getComputedStyle(addBookForm, null) : addBookForm.currentStyle;
-  addBookForm.style.visibility = visiblilityNextStatus[style.visibility];
-}
-
-// window addBookBtn click
-// set form visible
-const addBookBtn = document.getElementById('add-book-btn');
-addBookBtn.addEventListener('click', () => exchangeVisibility());
-
-// window submitBtn
-// call addBookToLibrary
-// call render
-const submitBtn = document.getElementById('submit-btn');
-submitBtn.addEventListener('click', () => {
-  const book = getBookInfoFromForm();
-  addBookToLibrary(book);
-  exchangeVisibility();
-  render();
-});
-
-// window ready
-// call render
-// in case the document is already rendered
-if (document.readyState !== 'loading') render();
-// modern browsers
-else if (document.addEventListener) document.addEventListener('DOMContentLoaded', render);
-// IE <= 8
-else {
-  document.attachEvent('onreadystatechange', () => {
-    if (document.readyState === 'complete') render();
-  });
-}
+myLibrary.resetBooks();
+uiController.render();
